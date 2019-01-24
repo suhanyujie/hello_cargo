a7afc8541eb5e5719222efcb19658af3572d93d4
suhanyujie <suhanyujie@qq.com>
2019-01-24 19:17:56 +0800

    Rust的struct

--- src/struct/struct1.rs
+++ src/struct/struct1.rs
@@ -19,6 +19,36 @@ fn main(){
     };
     println!("{}", user1.name);
     println!("{}", user2.height);
+    let mut rec = Rectangle {
+        width:21,
+        height:19,
+    };
+    println!("{:#?}", rec);
+    println!("1 the rectangle area is :{}\n", rec.area());
+    // 改变rectangle的属性
+    rec.change_width();
+    println!("2 the rectangle area is :{}\n", rec.area());
+    
 
     println!("hello world...");
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height:u32,
+}
+
+impl Rectangle {
+    fn area(&self)->u32 {
+        self.width * self.height
+    }
+
+    fn change_width(&mut self){
+        self.width = self.width + 2;
+    }
+}
+
+fn area(rec:Rectangle)->u32 {
+    rec.width * rec.height
 }
