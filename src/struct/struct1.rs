a008550cfab8c689a8f0dc5bbe7ef9764840ae36
suhanyujie <suhanyujie@qq.com>
2019-04-23 18:54:35 +0800

    review the struct

--- src/struct/struct1.rs
+++ src/struct/struct1.rs
@@ -7,7 +7,7 @@ struct User {
 
 fn main(){
     let user1 = User {
-        name:String::from("suhanyu"),
+        name:String::from("Samthon"),
         age:26,
         height:168,
     };
@@ -29,6 +29,9 @@ fn main(){
     rec.change_width();
     println!("2 the rectangle area is :{}\n", rec.area());
     
+    // 通过参数名和属性名一致，来简化实例化一个结构体
+    let user1 = build_user(String::from("Mark"), 28);
+    println!("mark struct is:{:?}\n", user1);
 
     println!("hello world...");
 }
@@ -49,6 +52,30 @@ impl Rectangle {
     }
 }
 
+#[allow(dead_code)]
 fn area(rec:Rectangle)->u32 {
     rec.width * rec.height
 }
+
+#[allow(dead_code)]
+fn build_user(name:String, age:i8) -> User {
+    User{
+        name,
+        age,
+        height:170
+    }
+}
+
+/*
+## 结构体
+* 定义结构体，需要使用 struct 关键字并为整个结构体提供一个名字。结构体的名字需要描述它所组合的数据的意义。
+* 一旦定义了结构体后，为了使用它，通过为每个字段指定具体值来创建这个结构体的 实例
+
+### 元组结构体
+* 元组结构体有着结构体名称提供的含义，但没有具体的字段名，只有字段的类型。
+
+### 类单元结构体
+* 我们也可以定义一个没有任何字段的结构体！它们被称为 类单元结构体（unit-like structs）因为它们类似于 ()，即 unit 类型。
+
+
+*/
--- src/study/ownership.rs
+++ src/study/ownership.rs
@@ -1,14 +1,56 @@
-
 fn main() {
     let s = String::from("hello world....");
     println!("{}", calculate_length(&s));
-}
+    
+    // let res = get_first_word(&s);
+    // println!("{}", res);
 
+    // string_slice();
+
+    println!("{}", get_first_word(&s));;
+}
 
 fn calculate_length(s: &String) -> usize {
     s.len()    
 }
 
+// 返回字符串中的第一个单词的位置
+#[test]
+fn get_first_word_size(s: &String) -> usize {
+    let bytes = s.as_bytes();
+    for (i,&item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+// 字符串slice
+#[allow(dead_code)]
+fn string_slice() {
+    let s = String::from("hello world sam");
+    // 包含end
+    let s1 = &s[0..=1];
+    // 不包含end   如果想要从第一个索引（0）开始，可以不写两个点号之前的值
+    let s2 = &s[0..1];
+    let s3 = &s[..1];
+    println!("string slice s1 is:{}\ns2 is :{}\n", s1, s2);
+    println!("s3 is:{}\n", s3);
+}
+
+// 返回字符串中的第一个单词
+fn get_first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i,&item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..=i];
+        }
+    }
+
+    &s[..]
+}
 
 /*
 ## 所有权
@@ -18,9 +60,20 @@ fn calculate_length(s: &String) -> usize {
     * 当所有者（变量）离开作用域，这个值将被丢弃。
 * 变量的所有权总是遵循相同的模式：将值赋给另一个变量时移动它。当持有堆中数据值的变量离开作用域时，其值将通过 drop 被清理掉，除非数据被移动为另一个变量所有。
 
+## 引用和借用
+* 可以使用大括号来创建一个新的作用域，以允许拥有多个可变引用，只是不能 同时 拥有
+* 不能在拥有不可变引用的同时拥有可变引用
+* “字符串 slice” 的类型声明写作 `&str`，它是一个`不可变引用`
+
+### 总结
+* 在任意给定时间，要么 只能有一个可变引用，要么 只能有多个不可变引用。
+* 引用必须总是有效。
+
+### 悬垂指针
+* 所谓悬垂指针是其指向的内存可能已经被分配给其它持有者
+
 ## 一些概念
 * 两次释放（相同）内存会导致内存污染，它可能会导致潜在的安全漏洞。
 * 像整型这样的在编译时已知大小的类型被整个存储在栈上，所以拷贝其实际的值是快速的
-
 
 */
