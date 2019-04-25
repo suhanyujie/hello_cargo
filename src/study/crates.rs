aa2950726dba7ddbec0802aab3eb6ba1ab1002c9
suhanyujie <suhanyujie@qq.com>
2019-04-25 15:50:07 +0800

    mod export

--- src/study/crates.rs
+++ src/study/crates.rs
@@ -1,7 +1,27 @@
-use crate::testCrate;
+// use self::testCrate::testCrate;
+
+pub mod instru {
+    pub fn i_fun1() {
+        println!("this is instru-{:#?}", "s_fun1");
+    }
+}
+
+mod sound {
+    pub use instru;
+    pub fn s_fun1() {
+        instru::i_fun1();
+        println!("this is sound-{:#?}", "s_fun1");
+    }
+}
+
+// 使用路径嵌套，来消除大量的 use 行
+use crate::{instru as instru1,sound as sound1};
 
 fn main() {
-    testCrate.second_fn();
+    // testCrate.second_fn();
+    sound::s_fun1();
+    // 这样使用，需要`重导出名称`
+    sound::instru::i_fun1();
 }
 
 /*
@@ -22,5 +42,18 @@ fn main() {
 * 增加 pub 关键字使得模块变为公有
 * 对于没有 pub 关键字的项，当你从当前模块向 “下” 看时是私有的
 * 使得模块公有并不使其内容也是公有的。模块上的 pub 关键字允许其父模块引用它。
+* 在结构体定义中使用 pub，可以使结构体公有。然而结构体的字段仍是私有的。
+* 而枚举则有所不同，它被设计为公有，则会使其所有成员公有
+
+### 同名类型
+* 将两个同名类型引入同一作用域时，可以使用`as`来讲其重命名，以避免冲突，例如：
+
+```
+use std::fmt::Result;
+use std::io::Result as IoResult;
+```
+
+
+
 
 */
